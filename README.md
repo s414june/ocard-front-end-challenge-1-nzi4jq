ace30275ffa0e7f5dd6691baf17fffae28b06d9b
s414june <s414june@gmail.com>
2021-06-30 20:42:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ocard-front-end-challenge-1-nzi4jq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ocard-front-end-challenge-1-nzi4jq)
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  metaInfo:{
+    meta:[
+      {
+        property:"tracker:google-analytics",
+        content:"UA-XXXXX-Y"
+      },
+      {
+        property:"tracker:google-tag-manager",
+        content:"GTM-XXXX"
+      },
+      {
+        property:"tracker:facebook-pixel",
+        content:"123456789010"
+      }
+    ]
+    
+  }
+}
+
+</script>
+<script>
+  gtag('config', 'UA-12345678-1');
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
